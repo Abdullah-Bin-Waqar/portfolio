ea1cb0094c65bc26fcdc4864f4188a2682e5b314
Abdullah-Bin-Waqar <engr.abdullahbinwaqar@gmail.com>
2026-06-26 22:45:28 +0500

    Update portfolio project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/portfolio/', // Replace "portfolio" if your repo has a different name
+
   plugins: [react()],
+
   server: {
     port: 5173,
     proxy: {
